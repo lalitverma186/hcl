7c43cb39bcf5061be480b0ab5a6ce81901b5350a
Lalit Verma <lalit.verma44@gmail.com>
2020-11-07 10:47:56 +0530

    Add files via upload
    HCL Test

---
+++ Virtual-Hackathon/src/main/java/com/hcl/hacathon/VirtualHackathon/VirtualHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.hacathon.VirtualHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualHackathonApplication.class, args);
+	}
+
+}
---
+++ Virtual-Hackathon/src/test/java/com/hcl/hacathon/VirtualHackathon/VirtualHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.hacathon.VirtualHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
